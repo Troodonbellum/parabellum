8d610667fb870093981f954a96fca49475cde44f
Romar02 <44519973+Romar02@users.noreply.github.com>
2018-10-27 12:28:51 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -23,6 +23,7 @@ minetest.register_node("parabellum:trophy", {
 	buildable_to = true,
 	groups = {snappy = 3, attached_node = 1},
 	sounds = default.node_sound_metal_defaults(),
+	light_source = 14,
 	selection_box = {
 		type = "fixed",
 		fixed = {-0.3125, -0.5, -0.1875, 0.3125, 0.125, 0.1875},
