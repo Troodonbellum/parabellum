a4af1dc489744d119c036c3aa981c3dbbd1a78bd
Romar02 <44519973+Romar02@users.noreply.github.com>
2018-11-14 21:58:26 +0100

    Update paint.lua

--- paint.lua
+++ paint.lua
@@ -14,3 +14,19 @@ minetest.register_node("parabellum:paint", {
 	},
 groups = {attached_node = 1, oddly_breakable_by_hand = 1, snappy = 3}
 })
+
+minetest.register_node("parabellum:banner", {
+	description = "Banner of the Parabellum server",
+	drawtype = "signlike",
+	tiles = {"parabellum_small_paint.png"},
+	inventory_image = "parabellum_small_paint.png",
+	wield_image = "parabellum_small_paint.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	selection_box = {
+		type = "wallmounted",
+	},
+groups = {attached_node = 1, oddly_breakable_by_hand = 1, snappy = 3}
+})
