1244c9573a28859f977bba354e9f6614c41f2ac6
Romar02 <44519973+Romar02@users.noreply.github.com>
2018-12-26 21:15:53 +0100

    Update paint.lua

--- parabellum_nodes/paint.lua
+++ parabellum_nodes/paint.lua
@@ -30,3 +30,99 @@ minetest.register_node("parabellum:banner", {
 	},
 groups = {attached_node = 1, oddly_breakable_by_hand = 1, snappy = 3}
 })
+minetest.register_node("parabellum:paint", {
+	description = "Picture of the Parabellum server",
+	drawtype = "signlike",
+	tiles = {"parabellum_paint.png"},
+	visual_scale = 1.90,
+	inventory_image = "parabellum_paint.png",
+	wield_image = "parabellum_paint.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	selection_box = {
+		type = "wallmounted",
+	},
+groups = {attached_node = 1, oddly_breakable_by_hand = 1, snappy = 3},
+})
+
+minetest.register_node("parabellum:banner", {
+	description = "Banner of the Parabellum server",
+	drawtype = "signlike",
+	tiles = {"parabellum_small_paint.png"},
+	inventory_image = "parabellum_small_paint.png",
+	wield_image = "parabellum_small_paint.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	selection_box = {
+		type = "wallmounted",
+	},
+groups = {attached_node = 1, oddly_breakable_by_hand = 1, snappy = 3}
+})
+
+minetest.register_node("parabellum:bronze_box", {
+	description = "Bronze Box",
+	drawtype = "signlike",
+	tiles = {"bronze_box.png"},
+	inventory_image = "bronze_box.png",
+	wield_image = "bronze_box.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	selection_box = {
+		type = "wallmounted",
+	},
+groups = {attached_node = 1, oddly_breakable_by_hand = 1, snappy = 3}
+})
+
+minetest.register_node("parabellum:iron_box", {
+	description = "Iron Box",
+	drawtype = "signlike",
+	tiles = {"iron_box.png"},
+	inventory_image = "iron_box.png",
+	wield_image = "iron_box.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	selection_box = {
+		type = "wallmounted",
+	},
+groups = {attached_node = 1, oddly_breakable_by_hand = 1, snappy = 3}
+})
+
+minetest.register_node("parabellum:palladium_box", {
+	description = "Palladium Box",
+	drawtype = "signlike",
+	tiles = {"palladium_box.png"},
+	inventory_image = "palladium_box.png",
+	wield_image = "palladium_box.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	selection_box = {
+		type = "wallmounted",
+	},
+groups = {attached_node = 1, oddly_breakable_by_hand = 1, snappy = 3}
+})
+
+minetest.register_node("parabellum:ulti_box", {
+	description = "Ulti Box",
+	drawtype = "signlike",
+	tiles = {"ulti_box.png"},
+	inventory_image = "ulti_box.png",
+	wield_image = "ulti_box.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	selection_box = {
+		type = "wallmounted",
+	},
+groups = {attached_node = 1, oddly_breakable_by_hand = 1, snappy = 3}
+})
