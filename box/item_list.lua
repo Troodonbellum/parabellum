3c492ada15b0e209be578c5ef4b06c40a937c28d
Romar02 <44519973+Romar02@users.noreply.github.com>
2018-12-28 22:41:51 +0100

    Update item_list.lua

--- box/item_list.lua
+++ box/item_list.lua
@@ -37,6 +37,8 @@ item_bronze = {
 	"currency:minegeld_50 100"
 	}
 item_steel = {
+	--Pour chaque : 16,66%
+	"nalc_default:acid_source 30",
 	"grenade:dynamitebig 100",
 	"bobblocks:trap_spike_major 150",
 	"default:obsidian 300",
@@ -44,23 +46,29 @@ item_steel = {
 	"xdecor:cobweb 500"
 }
 item_palladium = {
+	--Lingot d'unobtanium : 4%
 	"dmobs:unobtanium_bar",
+	--Lingot orichalcum : 8%
 	"xtraores:orichalcum_bar 10",
 	"xtraores:orichalcum_bar 10",
+	--Power Banner : 16%
 	"banners:power_banner",
 	"banners:power_banner",
 	"banners:power_banner",
 	"banners:power_banner",
+	--Lingot de palladium : 16%
 	"xtraores:palladium_bar 5",
 	"xtraores:palladium_bar 5",
 	"xtraores:palladium_bar 5",
 	"xtraores:palladium_bar 5",
+	--Tête de wither : 24%
 	"parabellum:head_wither_skeleton 9",
 	"parabellum:head_wither_skeleton 9",
 	"parabellum:head_wither_skeleton 9",
 	"parabellum:head_wither_skeleton 9",
 	"parabellum:head_wither_skeleton 9",
 	"parabellum:head_wither_skeleton 9",
+	--Billet de 50 : 32%
 	"currency:minegeld_50 120",
 	"currency:minegeld_50 120",
 	"currency:minegeld_50 120",
@@ -71,27 +79,34 @@ item_palladium = {
 	"currency:minegeld_50 120"
 }
 item_ulti = {
+	--Lingot d'ultigemite : 6,66%
 	"xtraores:ultigemite_bar",
 	"xtraores:ultigemite_bar",
+	--Lingot d'orichalcum : 10%
 	"xtraores:orichalcum_bar 10",
 	"xtraores:orichalcum_bar 10",
 	"xtraores:orichalcum_bar 10",
+	--Lingot d'unobtanium : 13?33%
 	"dmobs:unobtanium_bar",
 	"dmobs:unobtanium_bar",
 	"dmobs:unobtanium_bar",
 	"dmobs:unobtanium_bar",
+	--Power Banner : 20%
 	"banners:power_banner",
 	"banners:power_banner",
 	"banners:power_banner",
 	"banners:power_banner",
 	"banners:power_banner",
 	"banners:power_banner",
+	--Tête de wither : 23,33
 	"parabellum:head_wither_skeleton 12",
 	"parabellum:head_wither_skeleton 12",
 	"parabellum:head_wither_skeleton 12",
 	"parabellum:head_wither_skeleton 12",
 	"parabellum:head_wither_skeleton 12",
 	"parabellum:head_wither_skeleton 12",
+	"parabellum:head_wither_skeleton 12",
+	--Billet de 50 : 26,66
 	"currency:minegeld_50 140",
 	"currency:minegeld_50 140",
 	"currency:minegeld_50 140",
